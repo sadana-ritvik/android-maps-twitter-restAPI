49aa0b2ae4b36fc3d71ce172fae9fdb2ac83cd62
sadana-ritvik <ritviksadana@gmail.com>
2015-10-12 00:25:39 +0530

    Fix untracked files

--- app/src/main/java/com/android/tweetmap/variableClass.java
+++
@@ -1,9 +0,0 @@
-package com.android.tweetmap;
-
-/**
- * Created by DELL PC on 10/12/2015.
- */
-public class variableClass {
-    public static String KEY = "";
-    public static String SECRET = "";
-}
